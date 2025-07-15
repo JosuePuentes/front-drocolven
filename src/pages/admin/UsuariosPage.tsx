8c710ba7ac09880449f4e2cc7141b51d4d6c5426
ispulzar <stan.stalinprogramado@gmail.com>
2025-07-15 09:23:55 -0400

    actualizacion

--- src/pages/admin/UsuariosPage.tsx
+++ src/pages/admin/UsuariosPage.tsx
@@ -76,7 +76,7 @@ const UsuariosPage = () => {
                     />
                 </HasModule>
 
-                <HasModule module="usuarios">
+                <HasModule module="crear-clientes">
                     <CardModule
                         bottomText="Crear Cliente"
                         description="Registra nuevos clientes para el sistema de ventas."

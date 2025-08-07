d04bb458ce47f7c48eeb5e666f15cad2ffc339cc
ispulzar <stan.stalinprogramado@gmail.com>
2025-08-07 11:04:15 -0400

    actualizacion buscar codigo pedido

--- src/components/AdminCompras/filters.ts
+++ src/components/AdminCompras/filters.ts
@@ -1,11 +1,12 @@
 // src/utils/filters.ts
-export function filtrarPorMultiplesPalabrasAND<T>(data: T[], textoBusqueda: string, campos: (keyof T)[]): T[] {
+export function filtrarPorMultiplesPalabrasAND<T>(data: T[], textoBusqueda: string, campos: string[]): T[] {
   const palabrasBusqueda = textoBusqueda.toLowerCase().split(" ").filter(Boolean);
   return data.filter((item) =>
     palabrasBusqueda.every((palabra) =>
-      campos.some((campo) =>
-        String(item[campo]).toLowerCase().includes(palabra)
-      )
+      campos.some((campo) => {
+        const valor = (item as any)?.[campo];
+        return valor && String(valor).toLowerCase().includes(palabra);
+      })
     )
   );
 }
